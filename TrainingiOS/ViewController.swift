242081afea6dd71252324911a64dfa5a18339f97
W.VErtex <galaxy000213@gmail.com>
2018-06-30 21:34:28 +0900

    Initial Commit

---
+++ TrainingiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrainingiOS
+//
+//  Created by 이병찬 on 2018. 6. 30..
+//  Copyright © 2018년 Woolim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
